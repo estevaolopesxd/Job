4abc05386bbc24a3551087e7735b13f42c6969fe
Rodrigo lopes <rodrigoleiro@gmail.com>
2023-04-04 23:29:57 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// pages
-import Home from './Pages/Home/Home';
-import Cadastro from './Pages/Cadastro/Cadastro'
-=======
 
 // pages
 import Home from './Pages/Home/Home';
@@ -10,19 +5,17 @@ import Cadastro from './Pages/Cadastro/Cadastro'
 import Lista from './Pages/Lista/Lista'
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
->>>>>>> 0be191a5b9fa4597f7805c5441ce78015fb592a6
 
 function App() {
   return (
     <div className="App">
 
-<<<<<<< HEAD
 
       <Home />
 
       <Cadastro />
 
-=======
+
       <BrowserRouter>
         <Routes>
 
@@ -31,7 +24,7 @@ function App() {
           <Route path="/lista" element={<Lista />} />
         </Routes>
       </BrowserRouter>
->>>>>>> 0be191a5b9fa4597f7805c5441ce78015fb592a6
+
     </div>
 
   );
